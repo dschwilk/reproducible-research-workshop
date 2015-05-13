60b1369d2ca34053f108b8baedfc5969396c3153
Dylan W. Schwilk <dylan@schwilk.org>
2015-05-13 15:51:42 -0500

    Strip whitespace when reading data

--- course-materials/examples/clean-birdd.R
+++ course-materials/examples/clean-birdd.R
@@ -10,7 +10,7 @@ library(ggplot2)
 ### Clean up data:
 ##################
 birdd <- read.csv("http://bioquest.org/birdd/Morph_for_Sato.txt",
-                  sep="\t", stringsAsFactors=FALSE)
+                  sep="\t", stringsAsFactors=FALSE, strip.white=TRUE)
 
 names(birdd) <- tolower(names(birdd)) # make columns names lowercase
 birdd <- subset(birdd, islandid == "Flor_Chrl") # take only one island
@@ -20,9 +20,12 @@ names(birdd)[1:2] <- c("island", "taxon")
 birdd <- data.frame(na.omit(birdd)) # remove all rows with any NAs
 birdd$sex <- factor(birdd$sex) # remove extra remaining factor levels
 row.names(birdd) <- NULL # tidy up the row names
+
+
 # Quick look at wingl:
-ggplot(birdd, aes(taxon, wingl)) + geom_boxplot() + coord_flip()
+ggplot(birdd, aes(x=taxon, y=wingl)) + geom_boxplot() + coord_flip()
 # hm, anything wrong?
 
-ggplot(birdd, aes(wingl, beakh, color=taxon)) + geom_point()
+#ggplot(birdd, aes(x=wingl, y=beakh, color=taxon)) + geom_point() + 
+#  facet_grid(sex ~ ., scales="free")
 
